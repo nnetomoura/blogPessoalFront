b3e0eea7334da1df192f46bfe38954c3f804d346
Claudio Pereira <claudio.prepara0@gmail.com>
2022-06-03 01:03:37 -0300

    adicionados os botões da página de início (nova postagem e ver postagem) + tag de saudações ao usuário

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    if(environment.token == ""){
+      //alert("Sessão encerrada! Faça login novamente.")
+      this.router.navigate(["/entrar"])
+    }
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -18,6 +19,12 @@ export class AuthService {
   }
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario> ('https://blogpessoalclaudio.herokuapp.com/usuarios/cadastrar', usuario)
-
+  }
+  logado() {
+    let ok = false 
+    if(environment.token != ""){
+        ok = true
+    }
+    return ok 
   }
 }
