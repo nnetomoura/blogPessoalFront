3b39fe16477fdb0a4676441a8f7c2c90c1fd05bf
Claudio Pereira <claudio.prepara0@gmail.com>
2022-06-26 15:13:02 -0300

    inserção do método getByIdTema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -15,12 +15,24 @@ export class TemaService {
     headers: new HttpHeaders().set("Authorization", environment.token)
   }
 
-  getAllTema(): Observable<Tema[]> {
+  getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>("https://blogpessoalclaudio.herokuapp.com/temas", this.token)
   }
-
-  postTema(tema: Tema): Observable<Tema> {
-    return this.http.post<Tema>("https://blogpessoalclaudio.herokuapp.com/temas", tema, this.token)
-
+  getByIdTema(id:number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoalclaudio.herokuapp.com/temas/${id}`, this.token)
   }
+  
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>("https://blogpessoalclaudio.herokuapp.com/temas", tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>("https://blogpessoalclaudio.herokuapp.com/temas", tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogpessoalclaudio.herokuapp.com/temas/${id}`, this.token)
+  }
+
+
 }
