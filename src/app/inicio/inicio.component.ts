82de7597e7741254a48c273b8ec9d5c04a97dcd7
Claudio Pereira <claudio.prepara0@gmail.com>
2022-06-27 18:38:44 -0300

    implementação do método de postagem (POST) do CienciaBlog finalizada

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,7 +1,12 @@
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
-import { AuthService } from '../service/auth.service';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -10,16 +15,66 @@ import { AuthService } from '../service/auth.service';
 })
 export class InicioComponent implements OnInit {
 
+  
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  idUsuario = environment.id
+  usuario: Usuario = new Usuario()
+ 
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    
   ) { }
 
   ngOnInit() {
-
+    window.scroll(0,0)
     if(environment.token == ""){
       //alert("Sessão encerrada! Faça login novamente.")
       this.router.navigate(["/entrar"])
     }
+    this.getAllTemas()
   }
 
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp:Tema[])=>{
+      this.listaTemas = resp
+    })
+  
+  }
+  
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+  publicar(){
+    this.postagem.tema = this.tema
+
+    //this.tema.id = this.idTema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario 
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert('Postagem efetuada com sucesso!')
+      this.postagem = new Postagem()
+      this.getPostagens()
+    })
+  }
+
+  getPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
 }
